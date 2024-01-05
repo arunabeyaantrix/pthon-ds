162be091e35680c36c7668f10aed1a08ecdd2094
Arun dev <arun.dev@cast-er.com>
2024-01-05 16:34:47 +0530

    Completed liked list

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -68,6 +68,74 @@ class LinkedList:
         if self.length == 0:
             self.tail = None
         return temp
+    def get(self,index):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.head
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_value(self,index,value):
+        if index < 0 or index >= self.length:
+            return False
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return temp
+        else:
+            return False
+    
+    def insert(self,index, value):
+        if index < 0 or index > self.length:
+            return False
+        new_node = Node(value)
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        before = self.get(index - 1)
+        after = before.next
+        new_node.next = after
+        before.next = new_node
+        self.length += 1
+        return True
+
+    def remove(self,index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length -1:
+            return self.pop()
+        temp = self.get(index)
+        before = self.get(index - 1)
+        after = temp.next
+
+        before.next = after
+        temp.next = None
+        self.length -= 1
+        return temp
+    
+    def reverse(self):
+        if self.length = 0 or self.length == 1:
+            return True
+        before = None
+        temp = self.head
+        after = temp.next
+        self.head = self.tail
+        self.tail = temp
+
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+        return True
+
+
 
 my_ll = LinkedList(1)
 my_ll.append(2)
@@ -75,5 +143,7 @@ my_ll.append(3)
 my_ll.append(4)
 my_ll.append(5)
 my_ll.pop()
+
+print(my_ll.get(4).value)
 
 my_ll.print_list()
