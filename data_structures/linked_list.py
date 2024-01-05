69337a18fe29da7ec6585367fae0eb47b6b05611
Arun dev <arun.dev@cast-er.com>
2024-01-05 14:05:54 +0530

    New methods added

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -23,16 +23,57 @@ class LinkedList:
         if self.length == 0:
             self.head = new_node
             self.tail = new_node
-            return True
-        self.tail.next = new_node
-        self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
         self.length += 1
         return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        
+        temp = self.head
+        pre = temp
+
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        pre.next = None
+        self.length-= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = temp.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
 
 my_ll = LinkedList(1)
 my_ll.append(2)
 my_ll.append(3)
 my_ll.append(4)
 my_ll.append(5)
+my_ll.pop()
 
 my_ll.print_list()
